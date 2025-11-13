1b4810979b9d130fb97651e30cac8adb69465977
Aiden <ayasharian1@hwemail.com>
2025-11-13 13:25:12 -0800

    11/13 pt 1

---
+++ public/firebase-config.js
@@ -0,0 +1,22 @@
+// Copy your Firebase web app configuration here. You can find it in the Firebase console
+// under Project Settings > General > Your apps > Firebase SDK snippet.
+// Example:
+// export const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "your-project.firebaseapp.com",
+//   projectId: "your-project-id",
+//   storageBucket: "your-project.appspot.com",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJ96dvLP0fXVawMTTDRKy6WWabun0Sjj8",
+  authDomain: "personalwebsite-aiden.firebaseapp.com",
+  projectId: "personalwebsite-aiden",
+  storageBucket: "personalwebsite-aiden.firebasestorage.app",
+  messagingSenderId: "528136635879",
+  appId: "1:528136635879:web:bb1e5c4218f398b7b62942",
+  measurementId: "G-5QNJ35YCN7"
+};
+
